62b8a9182b616b2c1040e5125b31fee10b2cc256
N7A <arthurnicolas2@hotmail.fr>
2014-10-24 18:44:54 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -10,13 +10,15 @@ genTerrain(){
   chemin = Case(img, type);
   
   //generation chemin coherent
+    //case de depart au hasard
+    //do
+      //take une case autour au hasard
+    //while(nb de case accessible voulu)
   //completer trou par obs
   
-  int x=0;
   for (int i=0; i<50; i++){
     for (int j=0; j<50; j++){
-      map[x] = obs;
-      x++;
+      map[i][j] = obs;
     }
   }
 }
