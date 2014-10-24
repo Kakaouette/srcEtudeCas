ebd693d00454d10560d332b30f749269d524cc49
N7A <arthurnicolas2@hotmail.fr>
2014-10-24 18:32:12 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,6 +1,16 @@
 struct affichage{
-affichage(); // appellé pour générer le tout (ajouter argument pour plus d'options)
+affichage(); // appellÃ© pour gÃ©nÃ©rer le tout (ajouter argument pour plus d'options)
 genTerrain();
 genDepartArrivee();
 genObjet();
 };
+
+genTerrain(){
+  int x=0;
+  for (int i=0; i<50; i++){
+    for (int j=0; j<50; j++){
+      map[x] = Case(i,j);
+      x++
+    }
+  }
+}
