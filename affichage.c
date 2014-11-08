fa31c07ec1988929a4721090d52d3fd312462514
N7A <arthurnicolas2@hotmail.fr>
2014-11-08 16:39:48 +0100

    MAJ affichage

--- affichage.c
+++ affichage.c
@@ -1,68 +1,80 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <time.h>
-#include "Case.c"
+#include <math.h>
 
-struct affichage{
-affichage(); // appellé pour générer le tout (ajouter argument pour plus d'options)
-genTerrain();
-genDepartArrivee();
-genObjet();
-};
+#include "Jeu.h"
+#include "Case.h"
+#include "ressource.h"
+extern const int NB_CASE_X;
+extern const int NB_CASE_Y;
+extern const int SPRITE_WIDTH;
+extern const int SPRITE_HEIGHT;
 
-void Afficher(SDL_Surface * ecran)
+
+SDL_Rect decoupage(int x, int y){
+
+    SDL_Rect clip;
+
+    //clipping
+    clip.x = x*SPRITE_WIDTH; //0->15
+    clip.y = y*SPRITE_HEIGHT;
+    clip.w = SPRITE_WIDTH;
+    clip.h = SPRITE_HEIGHT;
+    return clip;
+}
+
+void afficher(SDL_Surface * ecran, Jeu * game)
 {
     //attributs de la feuille de sprites
-    const int SHEET_WIDTH = 512;
-    const int SHEET_HEIGHT = 512;
-    const int SPRITE_WIDTH = 32;
-    const int SPRITE_HEIGHT = 32;
+    const int SHEET_WIDTH = 512, SHEET_HEIGHT = 512;
     //nombre dimage: 512/32=16, 16*16
 
     //attributs de l'écran
-    const int SCREEN_WIDTH = 20*SPRITE_WIDTH;//640
-    const int SCREEN_HEIGHT = 15*SPRITE_HEIGHT;//480
+    const int SCREEN_WIDTH = NB_CASE_X*SPRITE_WIDTH;//640
+    const int SCREEN_HEIGHT = NB_CASE_Y*SPRITE_HEIGHT;//480
     const int SCREEN_BPP = 32;
 
     //Les surfaces
     SDL_Surface *textures = NULL;
-    //Les différentes parties de la feuille de sprites qui vont être blittés
-    SDL_Rect clip[ 4 ];
-    //clipping
-    //On coupe la partie en haut à gauche (premier sprite)
-    clip[ 0 ].x = 0*SPRITE_WIDTH; //0->15
-    clip[ 0 ].y = 0*SPRITE_HEIGHT;
-    clip[ 0 ].w = SPRITE_WIDTH;
-    clip[ 0 ].h = SPRITE_HEIGHT;
-
-    //herbe
-    clip[ 1 ].x = 15*SPRITE_WIDTH;
-    clip[ 1 ].y = 1*SPRITE_HEIGHT;
-    clip[ 1 ].w = SPRITE_WIDTH;
-    clip[ 1 ].h = SPRITE_HEIGHT;
-
-    //gems
-    clip[ 3 ].x = 10*SPRITE_WIDTH;
-    clip[ 3 ].y = 9*SPRITE_HEIGHT;
-    clip[ 3 ].w = SPRITE_WIDTH;
-    clip[ 3 ].h = SPRITE_HEIGHT;
-
-    Case c;
-    c.image = clip[1];
-
     textures = IMG_Load("Tiny32-Complete-Spritesheet-Repack3.png"); //load le tileset
+    //Les différentes parties de la feuille de sprites qui vont être blittés
+    SDL_Rect terre = decoupage(11, 4);
+    SDL_Rect herbe = decoupage(15, 1);
+    SDL_Rect perso = decoupage(10,12);
+    SDL_Rect gems = decoupage(10,9);
 
 
-    //On applique les sprites sur l'écran
-    apply_surface( 0, 0, textures, ecran, &clip[ 0 ] );
-    apply_surface( 1*SPRITE_WIDTH, 1*SPRITE_HEIGHT, textures, ecran, &clip[ 3 ] ); //0->(SCREEN_WIDTH/SPRITE_WIDTH)-1
+    Case cs[NB_CASE_X][NB_CASE_Y];
+    Case* c[NB_CASE_X][NB_CASE_Y];
+
     int i,j;
-    //On applique les sprites sur l'écran
-    for (i=0;i<SPRITE_WIDTH;i++){
-        for (j=0;j<SPRITE_HEIGHT;j++){
-            apply_surface( i*SPRITE_WIDTH, j*SPRITE_HEIGHT, textures, ecran, &c.image ); //0->(SCREEN_WIDTH/SPRITE_WIDTH)-1
+
+    for (i=0; i<NB_CASE_X;i++){
+        for (j=0; j<NB_CASE_Y;j++){
+            //cs[i][j].type = libre;
+            c[i][j]=&cs[i][j];
         }
     }
+
+
+
+    int y,x;
+    /**On applique les sprites sur l'écran*/
+
+    /**affichage map*/
+    for (y=0;y<NB_CASE_Y;y++){
+        for (x=0;x<NB_CASE_X;x++){
+            if (c[x][y]->type == libre){
+                c[x][y]->image = &terre;
+            }else{
+                c[x][y]->image = &herbe;
+            } //remplacer "c" par "game->map"
+            apply_surface( x*SPRITE_WIDTH, y*SPRITE_HEIGHT, textures, ecran, c[x][y]->image ); //0->(SCREEN_WIDTH/SPRITE_WIDTH)-1
+        }
+    }
+    /**affichage perso*/
+    //apply_surface( p1->i*SPRITE_WIDTH, p1->j*SPRITE_HEIGHT, textures, ecran, &p1->image );
 
 
     SDL_Flip(ecran); // Mise à jour de l'écran
@@ -70,8 +82,7 @@ void Afficher(SDL_Surface * ecran)
     SDL_FreeSurface(textures); // On libère la surface
 }
 
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip )//clip = NULL
-{
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip ){
     SDL_Rect offset;
 
     offset.x = x;
@@ -82,81 +93,123 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination,
 }
 
 int rand_a_b(int a, int b){
-    return rand()%(b-a) +a;
+    return rand() % (b - a) + a;
 }
 
-genTerrain(){
-    int nbCaseLibreVoulu = 10;
+void genTerrain(Case* map[][NB_CASE_Y]){
+    int nbCaseLibreVoulu = 1/3*(NB_CASE_X*NB_CASE_Y);
     int nbCaseTake = 0;
 
-    //put que d obs
-    for (int i=0; i<20; i++){
-        for (int j=0; j<15; j++){
-            map[i][j]->type = Type.infranchissable; //rendre la case infranchissable
+    int i,j;
+
+    for (i=0; i<20; i++){
+        for (j=0; j<15; j++){
+            map[i][j]->type = infranchissable; //rendre la case infranchissable
         }
     }
-
     //generation chemin coherent
     //case de depart au hasard
     srand(time(NULL)); // initialisation de rand
 	int	dX = rand_a_b(0,20);
 	int	dY = rand_a_b(0,15);
-	map[dX][dY]->type = Type.libre //rendre la case libre
+	map[dX][dY]->type = libre; //rendre la case libre
+    nbCaseTake ++;
 
-    int dX2, dY2
+        int xx,yy,nb, nbAnc;
+    do{//take une case autour au hasard
+        do{
+        nb=0;
+        for (xx = -1; xx <= 1; xx++) {
+			yy = abs(xx)-1;
+			if (((dX + xx)>=0) && ((dX + xx)<20) && ((dY + yy)>=0) && ((dY + yy)<15)) {
+			    if(map[dX + xx][dY + yy]->type != libre){
+                    nb++;
+			    }
+			}
+			yy = 1 - abs(xx);
+			if (yy != abs(xx)-1 && (dX + xx)>=0 && (dX + xx)<20 && (dY + yy)>=0 && (dY + yy)<15) {
+			    if(map[dX + xx][dY + yy]->type != libre){
+                    nb++;
+			    }
+			}
+		}
+        nbAnc = nb;
+		if(nbAnc == 0){
+            do{
+                dX = rand_a_b(0,20);
+                dY = rand_a_b(0,15);
 
-    do
-        //take une case autour au hasard
-        do
-            do
-                dX2 = dX + rand_a_b(-1,1);
-            while(dX2<0 || dX2>20)
-            do
-                dY2 = dY + rand_a_b(-1,1);
-            while(dY2<0 || dY2>15)
-            //while case pas dans terrain
-        while(map[dX2][dY2]->type = Type.libre)
-        //while case dej take (contain(caseChemin, caseij))
-        dX = dX2;
-        dY = dY2;
-        map[dX][dY]->type = Type.libre //rendre la case libre
-        nbCaseTake += 1;
-    while(nbCaseTake < nbCaseLibreVoulu) //nb de case accessible voulu
+            }while(map[dX][dY]->type != libre);
+		}
+        }while(nbAnc == 0);
 
-    //pb boucle infini si case sauto bloque
+        int q[nbAnc][2];
+        //case valide a coté de la case depart
+        for ( xx = -1; xx <= 1; xx++) {
+			yy = abs(xx)-1;
+			if ((dX + xx)>=0 && (dX + xx)<20 && (dY + yy)>=0 && (dY + yy)<15) {
+			    if(map[dX + xx][dY + yy]->type != libre){
+
+			    nb--;
+			    q[nb][0] = dX + xx;
+			    q[nb][1] = dY + yy;
+			    }
+			}
+			yy = 1 - abs(xx);
+			if (yy != abs(xx)-1 && (dX + xx)>=0 && (dX + xx)<20 && (dY + yy)>=0 && (dY + yy)<15) {
+			    if(map[dX + xx][dY + yy]->type != libre){
+
+			    nb--;
+			    q[nb][0] = dX + xx;
+			    q[nb][1] = dY + yy;
+			    }
+			}
+		}
+
+        int index = rand_a_b(0,nbAnc);
+        dX = q[index][0];
+        dY = q[index][1];
+        map[dX][dY]->type = libre; //rendre la case libre
+        nbCaseTake ++;
+    }while(nbCaseTake < nbCaseLibreVoulu); //nb de case accessible voulu
 }
 
-genDepartArrivee(){
+void genDepartArrivee(Case* map[][NB_CASE_Y]){
 
-    bool depart = false;
-    bool arrivee = false;
+    char depart = 0, arrivee = 0; //bool existe pas en c, c dla merde
 
     srand(time(NULL)); // initialisation de rand
-    int	dX, dY
-    while(depart == false or arrivee == false){
+    int	dX, dY;
+    do{
         //case au hasard
-        dX = rand_a_b(0,20);
-        dY = rand_a_b(0,15);
+        dX = rand_a_b(0,NB_CASE_X);
+        dY = rand_a_b(0,NB_CASE_Y);
 
-        if (map[dX][dY]->type == Type.libre){ //si case libre
+        if (map[dX][dY]->type == libre){ //si case libre
             if (depart){
-                map[dX][dY]->depart = 1;
-                depart = true;
+                map[dX][dY]->depart = pow(2,0);
+                depart = 1;
             }else{
-                map[dX][dY]->arrivee = 1;
-                arrivee = true;
+                map[dX][dY]->arrivee = pow(2,0);
+                arrivee = 1;
             }
         }
-    }
+    }while(depart == 0 || arrivee == 0);
 }
 
-genObjet(){
-    int nbRessourceVoulu = 2
-  while(nbRessource pas attein){
-    random i 0->50
-    random j 0->50
-    if (map[i][j] interrain){
-      map.ressource(i,j)
-    }
-  }
+void genObjet(int nbRessourceVoulu, Jeu * game){
+    int nbCaseTake = 0;
+    srand(time(NULL)); // initialisation de rand
+
+    int x,y;
+
+    while(nbCaseTake < nbRessourceVoulu){
+        do{
+            x= rand_a_b(0,NB_CASE_X);
+            y= rand_a_b(0,NB_CASE_Y);
+        }while(game->map[x][y]->type != libre);
+        game->ressources[nbCaseTake]->position[0]=x;
+        game->ressources[nbCaseTake]->position[1]=y;
+        nbCaseTake++;
+    }//pb pas de verif si case deja une ressource
 }
